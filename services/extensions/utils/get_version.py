2f49fbfad0cbaf4cb3ce182286bc7a62c366d23c
Luca Schiavoi <jerv.norsk@gmail.com>
2025-03-13 18:41:15 +0100

    chore(extensions): fixed workflow

--- services/extensions/utils/get_version.py
+++ services/extensions/utils/get_version.py
@@ -2,7 +2,8 @@ def git_version():
     import subprocess
 
     # result = subprocess.run(['git', 'tag', '--sort=-v:refname'], capture_output=True, text=True)
-    result = subprocess.run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], capture_output=True, text=True)
+    # result = subprocess.run(['git', 'rev-parse', '--abbrev-ref', 'HEAD'], capture_output=True, text=True)
+    result = subprocess.run('git describe --tags --abbrev=0'.split(" "), capture_output=True, text=True)
 
     version = result.stdout.strip().split('\n')[0].lstrip('v')
 
